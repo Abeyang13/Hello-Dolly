7df54c9188a5c42144d962ba32203a4b9fcaeb3f
iaiir <abeyang13@gmail.com>
2019-12-09 17:27:17 -0600

    Started on Dolly UI page.  Reworking some logic on app.JS GET request so that the page can load with all relevant messages

--- Client/app.js
+++ Client/app.js
@@ -1,29 +1,30 @@
-(function($){
-    function putForm( e ){
-        var info = {
-            Name : this["name"].value,
-            Text : this["text"].value,
-        };
+// (function($){
+//     function putForm( e ){
+//         var info = {
+//             Name : this["name"].value,
+//             Text : this["text"].value,
+//         };
 
-        $.ajax({
-            url: 'https://localhost:44388/api/messages',
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(info),
-            success: function( data, textStatus, jQxhr){
-                console.log("Success");             
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown);
-            }
-        });
-        e.preventDefault();
-    }
-    $('#message-form').submit( putForm );
-})(jQuery);
-
-//Dolly Grabs all messages when button is pressed by Dolly UI to show messages
+//         $.ajax({
+//             url: 'https://localhost:44388/api/messages',
+//             type: 'put',
+//             contentType: 'application/json',
+//             data: JSON.stringify(info),
+//             success: function( data, textStatus, jQxhr){
+//                 console.log("Success");        
+//             },
+//             error: function( jqXhr, textStatus, errorThrown ){
+//                 console.log( errorThrown);
+//             }
+//         });
+//         e.preventDefault();
+//     }
+//     $('#message-form').submit( putForm );
+// })(jQuery);
+document.getElementById("all-messages").innerHTML = "";
+//Dolly Grabs all messages that are from the past 10 minutes
 function getAllMessages(){
+    console.log();
     $.ajax({
         url: 'https://localhost:44388/api/messages',
         dataType: 'json',
@@ -36,10 +37,10 @@ function getAllMessages(){
                 row +=
                 "<tr><td>"
                 + obj.Name + "</td><td>"
-                + obj.Text + "</td><td>"
-                + "</td><tr>";
+                + obj.Text + "</td></tr>";
             });
             $("#all-messages").append(row);
         }
     });
 }
+$(document).ready(getAllMessages);
